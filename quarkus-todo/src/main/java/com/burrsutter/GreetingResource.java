9d03c47a15f9e4a651eac20b48cd8c6143d34815
Burr Sutter <burrsutter@gmail.com>
2022-10-15 14:28:11 -0400

    counter

--- quarkus-todo/src/main/java/com/burrsutter/GreetingResource.java
+++ quarkus-todo/src/main/java/com/burrsutter/GreetingResource.java
@@ -8,9 +8,17 @@ import javax.ws.rs.core.MediaType;
 @Path("/hello")
 public class GreetingResource {
 
+    int cnt = 1;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello from RESTEasy Reactive";
+    }
+
+    @GET
+    @Path("/stuff")
+    public String stuff() {
+        return "hello and more stuff: " + cnt++;
     }
 }
